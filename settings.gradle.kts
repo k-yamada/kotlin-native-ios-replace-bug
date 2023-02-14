be326e6885d793b69bee6a7dc3097ad774b8e929
Kazuhiro Yamada <yamadakazu45@gmail.com>
2023-02-14 11:31:06 +0900

    Initial commit

---
+++ settings.gradle.kts
@@ -0,0 +1,18 @@
+pluginManagement {
+    repositories {
+        google()
+        gradlePluginPortal()
+        mavenCentral()
+    }
+}
+
+dependencyResolutionManagement {
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "kotlin-native-ios-replace-bug"
+include(":androidApp")
+include(":shared")
