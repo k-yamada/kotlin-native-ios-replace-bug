0db9de467cbd849dd7452ed6dc61b4e8b76096d8
Kazuhiro Yamada <yamadakazu45@gmail.com>
2023-02-14 11:45:22 +0900

    Add reproduction code

--- shared/src/commonMain/kotlin/com/example/bug/Greeting.kt
+++ shared/src/commonMain/kotlin/com/example/bug/Greeting.kt
@@ -4,6 +4,8 @@ class Greeting {
     private val platform: Platform = getPlatform()
 
     fun greet(): String {
+        println("買っ".replace("掌", "X"))
+        println("買".replace("掌", "X"))
         return "Hello, ${platform.name}!"
     }
 }
